a4d8316a963ab6741cd4f699b3532ac4ab7762e0
Hannes Morgenthaler <hannes@morgethaler.ch>
2022-05-27 20:43:53 +0200

    trying to make yarn obey.

--- build/Tasks/DefaultTask.cs
+++ build/Tasks/DefaultTask.cs
@@ -1,4 +1,6 @@
-﻿using Cake.Frosting;
+﻿using Cake.Common;
+using Cake.Core.IO;
+using Cake.Frosting;
 using Cake.Yarn;
 
 namespace Build.Tasks
@@ -11,6 +13,18 @@ namespace Build.Tasks
       var viteJsProjDir = context.FileSystem
         .GetDirectory(context.SolutionDirectoryFullPath)
         .Path.Combine(context.ViteJsProjName);
+
+      context.StartAndReturnProcess("yarn", new ProcessSettings
+      {
+        WorkingDirectory = viteJsProjDir,
+        Arguments = "config set version-git-tag false"
+      });
+      context.StartAndReturnProcess("yarn", new ProcessSettings
+      {
+        WorkingDirectory = viteJsProjDir,
+        Arguments = "config set version-tag-prefix \"\""
+      });
+
       var yarnRunner = context.Yarn().FromPath(viteJsProjDir);
 
       yarnRunner.Install();
