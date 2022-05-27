74515b7c560972c72256e9c0da2cf5351d3e8e3d
Hannes Morgenthaler <hannes@morgethaler.ch>
2022-05-27 16:53:34 +0200

    use net6.0.

--- build/Program.cs
+++ build/Program.cs
@@ -6,27 +6,26 @@ using Cake.Frosting;
 
 public static class Program
 {
-    public static int Main(string[] args)
-    {
-        return new CakeHost()
-          .UseContext<BuildContext>()
-          .Run(args);
-    }
+  public static int Main(string[] args)
+  {
+    return new CakeHost()
+      .UseContext<BuildContext>()
+      .Run(args);
+  }
 }
 
 public class BuildContext : FrostingContext
 {
-    public bool Delay { get; set; }
+  public bool Delay { get; set; }
 
-    public BuildContext(ICakeContext context)
-      : base(context)
-    {
-        Delay = context.Arguments.HasArgument("delay");
-    }
+  public BuildContext(ICakeContext context)
+    : base(context)
+  {
+    Delay = context.Arguments.HasArgument("delay");
+  }
 }
 
 [TaskName("Default")]
-[IsDependentOn(typeof(WorldTask))]
 public class DefaultTask : FrostingTask
 {
 }
