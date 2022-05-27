0271ad9417d21a90f4108e0a5135bb604f51fa94
Hannes Morgenthaler <hannes@morgethaler.ch>
2022-05-27 20:14:13 +0200

    npm package somehow published.

--- build/BuildContext.cs
+++ build/BuildContext.cs
@@ -1,14 +1,26 @@
-﻿using Cake.Core;
+﻿using Build.Extensions;
+
+using Cake.Common.Solution;
+using Cake.Common.Tools.GitVersion;
+using Cake.Core;
+using Cake.Core.IO;
 using Cake.Frosting;
 
 namespace Build
 {
   public class BuildContext : FrostingContext
   {
-    public string YarnRoot { get; set; }
-
     public BuildContext(ICakeContext context) : base(context)
     {
+      (SolutionDirectoryFullPath, SolutionFile, Solution) = context.GetSolutionInfo();
+      ViteJsProjName = context.LoggedArgument(nameof(ViteJsProjName), "mumrich-vue-pinia-tools");
+      GitVersion = context.GitVersion();
     }
+
+    public GitVersion GitVersion { get; }
+    public SolutionParserResult Solution { get; }
+    public string SolutionDirectoryFullPath { get; }
+    public IFile SolutionFile { get; }
+    public string ViteJsProjName { get; set; }
   }
 }
--- build/Tasks/DefaultTask.cs
+++ build/Tasks/DefaultTask.cs
@@ -1,9 +1,27 @@
 ﻿using Cake.Frosting;
+using Cake.Yarn;
 
 namespace Build.Tasks
 {
   [TaskName("Default")]
-  public class DefaultTask : FrostingTask
+  public class DefaultTask : FrostingTask<BuildContext>
   {
+    public override void Run(BuildContext context)
+    {
+      var viteJsProjDir = context.FileSystem
+        .GetDirectory(context.SolutionDirectoryFullPath)
+        .Path.Combine(context.ViteJsProjName);
+      var yarnRunner = context.Yarn().FromPath(viteJsProjDir);
+
+      yarnRunner.Install();
+      yarnRunner.RunScript("build");
+      yarnRunner.Pack();
+      context.Yarn().Publish(settings =>
+      {
+        settings.WorkingDirectory = viteJsProjDir;
+        settings.NewVersion(context.GitVersion.Major, context.GitVersion.Minor, context.GitVersion.Patch);
+        settings.Tag("vue");
+      });
+    }
   }
 }
