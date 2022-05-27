33c3159859896d158dd3fb5b03845fa3dfa86349
Hannes Morgenthaler <hannes@morgethaler.ch>
2022-05-27 21:08:17 +0200

    add ts-support.

--- mumrich-vue-pinia-tools/vite.config.ts
+++ mumrich-vue-pinia-tools/vite.config.ts
@@ -1,12 +1,20 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import dts from "vite-plugin-dts";
 
 const libName = "mumrich-vue-pinia-tools";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    dts({
+      outputDir: "dist/types",
+      insertTypesEntry: true,
+      staticImport: true,
+    }),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "src/lib.ts"),
