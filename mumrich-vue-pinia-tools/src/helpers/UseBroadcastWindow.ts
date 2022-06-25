8251360e3439ac1bd7235ab54bc080ef1eacda6f
Hannes Morgenthaler <h.zbinden@ingtes.ch>
2022-06-25 22:03:15 +0200

    wip.

---
+++ mumrich-vue-pinia-tools/src/helpers/UseBroadcastWindow.ts
@@ -0,0 +1,9 @@
+export interface IUseBroadcastWindowOptions {}
+
+/**
+ * Reactive BroadcastWindow
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/Window/postMessage
+ * @param options
+ */
+export const useBroadcastWindow = (options: IUseBroadcastWindowOptions) => {};
