063049799c61c29d3a71318048968bbc3e53a4c0
Hannes Morgenthaler <hannes@morgethaler.ch>
2022-07-14 19:41:49 +0200

    working with yarp directly.

--- Vue.PiniaTools.Host/App/child-app/vite.config.ts
+++ Vue.PiniaTools.Host/App/child-app/vite.config.ts
@@ -3,17 +3,22 @@ import vue from "@vitejs/plugin-vue";
 
 const protocol = process.env.HMR_PROTOCOL ?? "ws";
 const port = Number(process.env.HMR_PORT) ?? 3000;
-const base = process.env.SERVER_BASE ?? "/";
+const base = process.env.SERVER_BASE ?? "/child-app/";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
+    host: "0.0.0.0",
     hmr: {
       protocol,
       port,
     },
     port: 8989,
-    base,
+    strictPort: true,
+    fs: {
+      strict: false,
+    },
   },
+  base,
 });
--- Vue.PiniaTools.Host/Program.cs
+++ Vue.PiniaTools.Host/Program.cs
@@ -3,20 +3,22 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 using Mumrich.SpaDevMiddleware.Contracts;
-using Mumrich.SpaDevMiddleware.Extensions;
 
 using Vue.PiniaTools.Host;
 
 var builder = WebApplication.CreateBuilder(args);
-var appHostSettings = builder.Configuration.Get<AppHostSettings>();
+//var appHostSettings = builder.Configuration.Get<AppHostSettings>();
 
 // AppSettings & variants
-builder.Services.AddSingleton(appHostSettings);
-builder.Services.AddSingleton<ISpaDevServerSettings>(appHostSettings);
+//builder.Services.AddSingleton(appHostSettings);
+//builder.Services.AddSingleton<ISpaDevServerSettings>(appHostSettings);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddCors();
-builder.RegisterSinglePageAppDevMiddleware(appHostSettings);
+
+var reverseProxyConfig = builder.Configuration.GetSection("ReverseProxy");
+// builder.RegisterSinglePageAppDevMiddleware(appHostSettings);
+builder.Services.AddReverseProxy().LoadFromConfig(reverseProxyConfig);
 
 var app = builder.Build();
 
@@ -24,6 +26,7 @@ app.UseStaticFiles();
 app.UseRouting();
 app.UseCors();
 app.MapControllers();
-app.MapSinglePageApps(appHostSettings);
+// app.MapSinglePageApps(appHostSettings);
+app.MapReverseProxy();
 
 app.Run();
